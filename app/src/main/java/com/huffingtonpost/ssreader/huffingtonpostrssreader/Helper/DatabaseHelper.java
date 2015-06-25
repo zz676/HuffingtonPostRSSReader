7ab96017c7eb65bb2020f607ef54fcf403302875
Zhisheng <zz676@nyu.edu>
2015-06-24 23:28:06 -0400

    init commit

---
+++ app/src/main/java/com/huffingtonpost/ssreader/huffingtonpostrssreader/Helper/DatabaseHelper.java
@@ -0,0 +1,7 @@
+package com.huffingtonpost.ssreader.huffingtonpostrssreader.Helper;
+
+/**
+ * Created by Zhisheng on 6/24/2015.
+ */
+public class DatabaseHelper {
+}
---
+++ app/src/main/java/com/huffingtonpost/ssreader/huffingtonpostrssreader/modules/Feed.java
@@ -0,0 +1,7 @@
+package com.huffingtonpost.ssreader.huffingtonpostrssreader.modules;
+
+/**
+ * Created by Zhisheng on 6/24/2015.
+ */
+public class Feed {
+}
